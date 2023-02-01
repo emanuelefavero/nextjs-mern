90c6bcb160c7cb2ddcd5a88a7a3cb8ed6a95ebaf
emanuelefavero <emafaux89@gmail.com>
2023-02-01 22:05:51 +0100

    add TODO in server.js

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ app.use(
 // Routes
 app.use('/api', routes)
 
+// ? TODO ? Serve static assets if in production ?
+
 // Start server
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
